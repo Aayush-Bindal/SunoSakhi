eb9a3f584b426cecee014a0fdc9d66914fe28a2f
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-06-21 17:10:47 +0530

    update main.py to make all services work

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 from services.stt import detect_language_from_audio
 from services.translate import translate_text
+from services.llm import generate_response
+from services.tts import text_to_speech
 
 # Set Google Cloud credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"         #API Key calling
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
-#stt
-audio_path = "input/2.wav"
+#start
+audio_path = "input/input.wav"
 transcript, lang_code = detect_language_from_audio(audio_path)
-translate_text = translate_text(transcript, source_language=lang_code, target_language='en')
+translated_text = translate_text(transcript, source_language=lang_code, target_language='en')
+response = generate_response(translated_text)
+translated_response = translate_text(response, source_language='en', target_language=lang_code)
+text_to_speech(translated_response, language_code=lang_code)
 
-print("Transcript:", transcript)
-print("Detected Language Code:", lang_code)
-print("Trans", translate_text)
+
+# for debugging
+# print("Transcript:", transcript)
+# print("Detected Language Code:", lang_code)
+# print("Trans:", translated_text)
+# print("response:", response)
+# print("output", translated_response)
