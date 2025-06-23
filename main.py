919da77b37db2a4b7a8f2bf095806cfb4226b320
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-06-23 17:42:02 +0530

    update main.py for llm2

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from utils.audio_utils import convert_to_wav_16khz_mono
 from services.stt import detect_language_from_audio
 from services.translate import translate_text
-from services.llm import generate_response
-# from services.llm2 import generate_response2
+# from services.llm import generate_response
+from services.llm2 import generate_response2
 from services.tts import text_to_speech
 
 # Set Google Cloud credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
 # Load environment variables from .env file
-# load_dotenv()
-# api_key = os.getenv("HF_TOKEN")
+load_dotenv()
+api_key = os.getenv("HF_TOKEN")
 
 #start
 audio_formats = ['.mp3', '.wav', '.m4a', '.flac', '.ogg', '.aac', '.wma']
@@ -30,8 +30,8 @@ if audio_path is None:
 converted_audio_path = convert_to_wav_16khz_mono(audio_path)
 transcript, lang_code = detect_language_from_audio(converted_audio_path)
 translated_text = translate_text(transcript, source_language=lang_code, target_language='en')
-response = generate_response(translated_text)
-# response = generate_response2("why are my periods late", api_key)
+# response = generate_response(translated_text)
+response = generate_response2(translated_text, api_key)
 translated_response = translate_text(response, source_language='en', target_language=lang_code)
 text_to_speech(translated_response, language_code=lang_code)
 
